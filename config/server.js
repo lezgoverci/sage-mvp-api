b3953582f2cc0234375ed61a70c03e261af98c30
Vercillius Mila, Jr <vercilliusjrmila@gmail.com>
2021-06-15 08:01:15 +0800

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21e75d951f551f1eb30f9c9332bc034c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '461b91a4-2837-49ab-be2f-2246a95562c1'
+};
